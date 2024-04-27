8ac3e6f565a1361c072889e7cfbc044c7016195f
trasherdk <trasherdk@gmail.com>
2024-04-27 08:09:16 +0200

    Restore test file

--- tests/store-ava.mjs
+++ tests/store-ava.mjs
@@ -1,17 +1,11 @@
-import process from 'node:process';
-import test, { registerCompletionHandler } from 'ava';
+import test from "ava";
 import { connection, wait } from "./helpers/util.mjs";
 import { WebSocketServer, WebSocket } from "ws";
 import websocketStore from "svelte-websocket-store";
-import { unsubscribe } from 'node:diagnostics_channel';
 
 globalThis.WebSocket = WebSocket;
 
 let port = 5002;
-
-registerCompletionHandler(() => {
-  process.exit();
-});
 
 test.beforeEach(async t => {
   port++;
@@ -88,12 +82,19 @@ test("several subscriptions", async t => {
   t.true(closeCalled);
 });
 
-test("failing subscription", async t => {
-  const store = await websocketStore("ws://localhost:9999", "INITIAL")
+test.skip("failing subscription", async t => {
+  try {
+    const store = websocketStore("ws://localhost:9999", "INITIAL");
+    const unsubscribe = store.subscribe(value => { });
 
-  console.log("store", store);
-  t.asserts(store.subscribe).is();
-  console.log("store", store.message);
+    store.set("FROM_CLIENT_1");
+
+    await wait(100);
+
+    t.true(false);
+  } catch (e) { }
+
+  t.true(true);
 });
 
 test("should not open a new ws connection if already existing", async t => {
